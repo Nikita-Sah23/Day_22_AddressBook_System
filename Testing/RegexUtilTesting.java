29a9abb1cf2a383fda1890aa7cfac1336229f189
Nikita-sah <nikitasah4gmail.com>
2023-03-07 22:53:53 +0530

    FIrst commit

---
+++ Testing/RegexUtilTesting.java
@@ -0,0 +1,84 @@
+package Testing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import Utility.RegexUtil;
+
+
+public class RegexUtilTesting 
+	{
+	    @Test
+	    public void testOneFirstName()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertFalse(obj.isStartWithCapitalLatter("nikita"));
+
+	    }
+	    @Test
+	    public void testTwoFirstName()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isStartWithCapitalLatter("Nikita"));
+
+	    }
+	    @Test
+	    public void testOneMobileNumber()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isCheckMobileNumber("704200366"));
+
+	    }
+	    @Test
+	    public void testTwoMobileNumber()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isCheckMobileNumber("6600222000"));
+
+	    }
+	    @Test
+	    public void testOneEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isCheckEmailId("abc@yahoo.com"));
+
+	    }
+	    @Test
+	    public void testTwoEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isCheckEmailId("abc-100@yahoo.com"));
+
+	    }
+	    @Test
+	    public void testThreeEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertTrue(obj.isCheckEmailId("abc.100@yahoo.com"));
+
+	    }
+	    @Test
+	    public void testFourEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertFalse(obj.isCheckEmailId("abc@%*.com"));
+
+	    }
+	    @Test
+	    public void testFiveEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertFalse(obj.isCheckEmailId("abc"));
+
+	    }
+	    @Test
+	    public void testSixEmailId()
+	    {
+	        RegexUtil obj = new RegexUtil();
+	        Assert.assertFalse(obj.isCheckEmailId("abc@gmail.com.aa.au"));
+
+	    }
+
+	
+
+}
